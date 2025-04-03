06afbb6f0275de0478b17504dd96d71f39d91ef2
Anton Remzov <ant.remzov@yandex.ru>
2025-04-03 18:31:52 +0300

    start

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"GolangAdvanced/internal/hello"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	router := http.NewServeMux()
+	hello.NewHelloHandler(router)
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is listening in port 8081")
+	server.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module GolangAdvanced
+
+go 1.23.6
