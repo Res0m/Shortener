20630d36b46e71aadce3d34648e9ba0155df7445
Anton Remzov <ant.remzov@yandex.ru>
2025-04-10 12:39:02 +0300

    Простейшая валидация данных

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -2,7 +2,8 @@ package auth
 
 import (
 	"GolangAdvanced/configs"
-	 res "GolangAdvanced/pkg/response"
+	res "GolangAdvanced/pkg/response"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -27,6 +28,22 @@ func (login *AuthHandler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		fmt.Println(login.Config.Auth.Secret)
 		fmt.Println("Login")
+		//Прочитать Body
+		var payload LoginRequest
+		err := json.NewDecoder(req.Body).Decode(&payload)
+		if err != nil {
+			res.JsonRes(w, err.Error(), 402)
+			return
+		}
+		if payload.Email == "" {
+			res.JsonRes(w, "Email required", 402)
+			return
+		}
+		if payload.Password == "" {
+			res.JsonRes(w, "Password required", 402)
+			return
+		}
+		fmt.Println(payload)
 		data := LoginResponse{
 			Token: "123",
 		}
